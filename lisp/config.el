d172ea764d7cc052e18d0d4c1f457ab4d660750e
Jappie Klooster <jappieklooster@hotmail.com>
2017-06-28 14:13:45 +0200

    Some unused lisp

--- lisp/config.el
+++ lisp/config.el
@@ -6,15 +6,6 @@
                          ("melpa" . "https://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(setq use-package-verbose t)
-(require 'use-package)
-;;; Org mode settings
-
-;;(use-package org :ensure t)
-;;(use-package htmlize :ensure t)
 
 ;;; Loading custom backend
 (add-to-list 'load-path "lisp/")
