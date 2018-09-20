6f5caf73d383d58ff545bdf82c73cab31693b224
Jappie Klooster <jappieklooster@hotmail.com>
2018-09-20 17:24:40 +0200

    Daisee resume

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,7 @@ AUTHOR_FEED_RSS = None
 LINKS_EX = [
     (
         'resume',
-        '/files/2018/resume-exol.pdf',
+        '/files/2018/resume-daisee.pdf',
         "To convince you to hire me!",
         'resume'
 
