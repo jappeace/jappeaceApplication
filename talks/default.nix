96b231cf6c3e1d7abd089d41c8c2b5760adec9b8
Jappie Klooster <jappieklooster@hotmail.com>
2021-04-24 17:15:26 -0400

    Run nix format

--- talks/default.nix
+++ talks/default.nix
@@ -1,28 +1,29 @@
 { pkgs ? import (builtins.fetchTarball {
-    # nixpkgs-unstable 2020.11.16
-    url = https://github.com/NixOS/nixpkgs/archive/bc714f86a515a558c2f595e1a090e882d08bd7ca.tar.gz;
-  }) {}
+  # nixpkgs-unstable 2020.11.16
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/bc714f86a515a558c2f595e1a090e882d08bd7ca.tar.gz";
+}) { }
 
 , revealjs ? pkgs.fetchFromGitHub {
-    owner = "hakimel";
-    repo = "reveal.js";
-    rev = "4.1.0";
-    sha256 = "10xhblbyw8mvak58d294hbxxnf5sq0akj6qldv7brgm6944zppm0";
-  }
- , org-files ? ["category-adts" "keter-nix" "mtl"  ]
- , theme ? "simple" # https://github.com/jgm/pandoc/wiki/Using-pandoc-to-produce-reveal.js-slides
+  owner = "hakimel";
+  repo = "reveal.js";
+  rev = "4.1.0";
+  sha256 = "10xhblbyw8mvak58d294hbxxnf5sq0akj6qldv7brgm6944zppm0";
+}, org-files ? [ "category-adts" "keter-nix" "mtl" ], theme ?
+  "simple" # https://github.com/jgm/pandoc/wiki/Using-pandoc-to-produce-reveal.js-slides
 }:
 let
-  file-paths = map (x: {path = ./. + "/${x}.org"; name = x;}) org-files;
-in
-# https://github.com/NixOS/nixpkgs/blob/5d8dd5c2598a74761411bc9bef7c9111d43d2429/pkgs/build-support/trivial-builders.nix#L42
-pkgs.runCommand "talks" {} (''
+  file-paths = map (x: {
+    path = ./. + "/${x}.org";
+    name = x;
+  }) org-files;
+  # https://github.com/NixOS/nixpkgs/blob/5d8dd5c2598a74761411bc9bef7c9111d43d2429/pkgs/build-support/trivial-builders.nix#L42
+in pkgs.runCommand "talks" { } (''
   set -xe
   mkdir -p $out
   cp -R ${./img} $out/img
   ln -s ${revealjs} $out/reveal.js
-  '' +
-    pkgs.lib.concatStrings(pkgs.lib.forEach file-paths (file: ''
-        ${pkgs.pandoc}/bin/pandoc -s -V theme=${theme} -t revealjs -o $out/${file.name}.html ${file.path}
-    '')))
-  # sed -i "s/plugins:\ \[/plugins: [ RevealHighlight, /" $out/${talk-name}.html
+'' + pkgs.lib.concatStrings (pkgs.lib.forEach file-paths (file: ''
+  ${pkgs.pandoc}/bin/pandoc -s -V theme=${theme} -t revealjs -o $out/${file.name}.html ${file.path}
+'')))
+# sed -i "s/plugins:\ \[/plugins: [ RevealHighlight, /" $out/${talk-name}.html
