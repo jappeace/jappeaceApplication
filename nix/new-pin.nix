39f57c190e4dead9ba7165bb0a95c97c4f9d791b
Jappie Klooster <jappieklooster@hotmail.com>
2025-09-10 21:31:21 -0400

    don't fetch entire repo, just the tarball

--- nix/new-pin.nix
+++ nix/new-pin.nix
@@ -1,7 +1,6 @@
-import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-pin-2018-06-20";
-    url = "https://github.com/nixos/nixpkgs/";
-    rev = "c36f81b42665d8f9849b5a39b5c05f115c39d50a";
-    ref = "release-22.11";
+import
+(builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-pin-2018-06-20";
+  url = "https://github.com/nixos/nixpkgs/archive/c36f81b42665d8f9849b5a39b5c05f115c39d50a.tar.gz";
 })
